3e9f2877aef8371ab043cb9f3989800e00cb863c
Ashley <ashley.carlson1997@gmail.com>
2020-04-03 15:16:15 -0600

    hopefully

--- project/layout/header.php
+++ project/layout/header.php
@@ -6,15 +6,15 @@
 
 	<title>Pathfinder Compendium</title>
 
-	<link rel="shortcut icon" href="../progressus/Progressus/assets/images/gt_favicon.png">
+	<link rel="shortcut icon" href="progressus/Progressus/assets/images/gt_favicon.png">
 
 	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
-	<link rel="stylesheet" href="../progressus/Progressus/assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../progressus/Progressus/assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="progressus/Progressus/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="progressus/Progressus/assets/css/font-awesome.min.css">
 
 	<!-- Custom styles for our template -->
-	<link rel="stylesheet" href="../progressus/Progressus/assets/css/bootstrap-theme.css" media="screen" >
-	<link rel="stylesheet" href="../progressus/Progressus/assets/css/main.css">
+	<link rel="stylesheet" href="progressus/Progressus/assets/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="progressus/Progressus/assets/css/main.css">
 
 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
