caf55f4ab8f1dbcb64c2b7c9d1a83ba847e284b3
Ashley <ashley.carlson1997@gmail.com>
2020-04-29 01:12:14 -0600

    test

--- project/characters.php
+++ project/characters.php
@@ -8,37 +8,37 @@
 <iframe src="pre-genchar/Amiri.pdf" width="80%" height="600em">
 </iframe>
 <h1>Ezren</h1>
-<iframe src="pre-genchar/Ezren.pdf" width="80%" height="600em" >
+<iframe src="pre-genchar/Ezren.pdf" width=80% height="600em" >
 </iframe>
 <h1>Fumbus</h1>
-<iframe src="pre-genchar/Fumbus.pdf" width="80%" height="600em" >
+<iframe src="pre-genchar/Fumbus.pdf" width=80% height="600em" >
 </iframe>
 <h1>Harsk</h1>
-<iframe src="pre-genchar/Harsk.pdf"  width="80%" height="600em">
+<iframe src="pre-genchar/Harsk.pdf">
 </iframe>
 <h1>Kyra</h1>
-<iframe src="pre-genchar/Kyra.pdf" width="80%" height="600em">
+<iframe src="pre-genchar/Kyra.pdf">
 </iframe>
 <h1>Lem</h1>
-<iframe src="pre-genchar/Lem.pdf" width="80%" height="600em">
+<iframe src="pre-genchar/Lem.pdf">
 </iframe>
 <h1>Lini</h1>
-<iframe src="pre-genchar/Lini.pdf"  width="80%" height="600em">
+<iframe src="pre-genchar/Lini.pdf">
 </iframe>
 <h1>Merisiel</h1>
-<iframe src="pre-genchar/Merisiel.pdf" width="80%" height="600em">
+<iframe src="pre-genchar/Merisiel.pdf">
 </iframe>
 <h1>Sajan</h1>
-<iframe src="pre-genchar/Sajan.pdf" width="80%" height="600em">
+<iframe src="pre-genchar/Sajan.pdf">
 </iframe>
 <h1>Seelah</h1>
-<iframe src="pre-genchar/Seelah.pdf" width="80%" height="600em">
+<iframe src="pre-genchar/Seelah.pdf">
 </iframe>
 <h1>Seoni</h1>
-<iframe src="pre-genchar/Seoni.pdf" width="80%" height="600em">
+<iframe src="pre-genchar/Seoni.pdf">
 </iframe>
 <h1>Valeros</h1>
-<iframe src="pre-genchar/Valeros.pdf" width="80%" height="600em">
+<iframe src="pre-genchar/Valeros.pdf">
 </iframe>
 <?php
 // add footer
