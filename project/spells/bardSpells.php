075b1708e9f04e33b8b7db5a9a52c232d23d5d29
Ashley <ashley.carlson1997@gmail.com>
2020-04-28 22:24:54 -0600

    test

--- project/spells/bardSpells.php
+++ project/spells/bardSpells.php
@@ -12,34 +12,7 @@
   </li>
   <li class="breadcrumb-item active">Bard Spells</li>
 </ol>
-<?php
-$m = new MongoClient();
-    $db = $m->selectDB('pathfinderDB');
-    $collection = $connection-><pathfinderDB>-><spells>
-    $filter = ["bard" => ["$gte" => 1]];
-    $results = $collection.find($filter);
 
-    $cursor = $results->find();
-    //echo "<html><head><body>";
-    echo "<table>";
-    foreach($cursor as $doc) {
-         echo "<tr>";
-            echo "<td>" . $doc['name'] . "</td>";
-            echo "<td>" . $doc['school'] . "</td>";
-            echo "<td>" . $doc['spell_level'] . "</td>";
-            echo "<td>" . $doc['casting_time'] . "</td>";
-            echo "<td>" . $doc['components'] . "</td>";
-            echo "<td>" . $doc['saving_throw'] . "</td>";
-            echo "<td>" . $doc['range'] . "</td>";
-            echo "<td>" . $doc['area'] . "</td>";
-            echo "<td>" . $doc['spell_resistence'] . "</td>";
-            echo "<td>" . $doc['duration'] . "</td>";
-            echo "<td>" . $doc['target'] . "</td>";
-            echo "<td>" . $doc['effect'] . "</td>";
-         echo "</tr>";
-    }
-    echo "<table>";
-?>
 <?php
 // add footer
 include("../layout/footer.php");
